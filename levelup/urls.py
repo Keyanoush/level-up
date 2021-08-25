df9466922ea79487d357246359731f8868e557e4
keyanoush <keyanoush@Keyanoush.localdomain>
2021-08-25 10:16:03 -0500

    tests and templates

--- levelup/urls.py
+++ levelup/urls.py
@@ -7,16 +7,19 @@ from levelupapi.views import (
     GameTypeView,
     GameView,
     EventView,
+    Profile,
 )
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'gametypes', GameTypeView, 'gametype')
 router.register(r'games', GameView, 'game')
 router.register(r'events', EventView, 'event')
+router.register(r'profile', Profile, 'profile')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register', register_user),
     path('login', login_user),
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('', include('levelupreports.urls')),
 ]
---
+++ levelupreports/views/connection.py
@@ -0,0 +1,2 @@
+class Connection:
+    db_path = "/home/keyanoush/workspace/python/levelup/db.sqlite3"
